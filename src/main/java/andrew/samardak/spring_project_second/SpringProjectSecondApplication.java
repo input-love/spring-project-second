8e30de538dbfcf2198d9a295dcc26eba4b01b48d
Andrew Samardak <andrew-mac@MacBook-Air-Andrew.local>
2024-11-24 04:27:45 +0300

    init commit

---
+++ src/main/java/andrew/samardak/spring_project_second/SpringProjectSecondApplication.java
@@ -0,0 +1,13 @@
+package andrew.samardak.spring_project_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectSecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrew/samardak/spring_project_second/SpringProjectSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package andrew.samardak.spring_project_second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
